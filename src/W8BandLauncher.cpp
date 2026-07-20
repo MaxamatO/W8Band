bb1a89a77f6d0116603e34f0faa4d6f907bb3358
MaxamatO <pierchala.maks@gmail.com>
2026-07-20 20:58:57 +0200

    TODO and a little cleanup

--- src/W8BandLauncher.cpp
+++ src/W8BandLauncher.cpp
@@ -10,23 +10,9 @@ W8BandLauncher::W8BandLauncher()
 
 bool W8BandLauncher::Initialize()
 {
-    Serial.println("Init1");
-    Serial.flush();
     m_ErrorMessage = "";
-    Serial.println("A");
-    Serial.flush();
-    // Wire.begin();
-    Serial.println("B");
-    Serial.flush();
-    delay(10);
-    Serial.println("C");
-    Serial.flush();
     pinMode(WAKEUP_INT1, INPUT_PULLDOWN);
     // attachInterrupt(digitalPinToInterrupt(WAKEUP_INT1), WakeUpISR1, RISING);
-    Serial.println("D");
-    Serial.flush();
-    delay(10);
-    Serial.println("Init after Wire.begin");
     if(!m_Lsm.InitLsm())
     {
         m_ErrorMessage = "Lsm init failed";
@@ -37,11 +23,6 @@ bool W8BandLauncher::Initialize()
         m_ErrorMessage = "Ble init failed";
         return false;
     }
-    Serial.println("E");
-    Serial.flush();
-
-    Serial.println("F");
-    Serial.flush();
     m_W8BandServiceManager.emplace(m_Ble, m_Lsm, m_DataContext);
     if(!m_W8BandServiceManager.has_value())
     {
@@ -49,8 +30,6 @@ bool W8BandLauncher::Initialize()
         return false;
     }
     m_W8BandServiceManager->Init();
-    Serial.println("G");
-    Serial.flush();
     return true;
 }
 
